b56e1c670475198e8964a6088d922b21c0555ce4
Filip Balder <fbalder1122@gmail.com>
2022-08-12 14:02:50 +0200

    Updated request script. Needs testing.

--- bash_variant/send_request.sh
+++ bash_variant/send_request.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
 
-command=$1
+ip_address=$1
+command=$2
 
 case $command in
 	"machineRegister")
 		linuxVersion=$(cat /proc/version | cut -d ' ' -f3)
-		echo $linuxVersion
-		echo "192.168.1.52/api/main/$command" ;;
+		name=$(hostname)
+
+		curl -X POST -H "Content-type: application/json" \
+			-d '{"name":$name, "linuxVersion":$linuxVersion}' \
+		 	https://$ip_address/api/main/machineRegister
+
+		echo "$ip_address/api/main/$command" ;;
 	"processRegister")
-		pid=$2
-		echo "192.168.1.52/api/main/$command" ;;
+		pid=$3
+
+		machineId=$() #mozda preko nekog filea pohraniti id koji vrati machineRegister?
+
+		processName=$(cat /proc/$pid/status | grep Name | cut -d$'\t' -f2)
+		processPath=$(ps -p $pid -o cmd | cut -d$'\n' -f2)
+
+		curl -X POST -H "Content-type: application/json" \
+			-d '{"processIdSystem":$pid, "name":$processName, "machineId":$machineId}' \
+			https://$ip_address/api/main/processRegister
+
+		echo "$ip_address/api/main/$command" ;;
 	"processUpdate")
-		pid=$2
-		echo "192.168.1.52/api/main/$command" ;;
+		pid=$3
+
+		state=$(cat /proc/$pid/status | grep Status | cut -d$'\t' -f2)
+		cpuUtil=$(ps -p $pid -o %cpu | cut -d$'\n' -f2)
+		memUtil=$(ps -p $pid -o %mem | cut -d$'\n' -f2)
+		threads=$(cat /proc/$pid/status | grep Threads | cut -d$'\t' -f2)
+
+		curl -X POST -H "Content-type: application/json" \
+			-d '{"processId":$pid, "state":$state, "cpuUtil":$cpuUtil, "threads":$threads}'
+			https://$ip_address/api/main/processUpdate
+
+		echo "$ip_address/api/main/$command" ;;
 	*)
 		echo "Unknown command!" ;;
 esac
