45380f6a68acd6a92b29661ba07660b01ddefa08
Filip Balder <fbalder1122@gmail.com>
2022-08-13 12:35:10 +0200

    Script now can update process.

--- bash_variant/send_request.sh
+++ bash_variant/send_request.sh
@@ -15,33 +15,68 @@ case $command in
 		printf "MachineId: %s\n" $newId > machineId
 
 		echo $newId$'\n'
-		echo "$ip_address/api/main/$command" ;;
+		echo "$ip_address/api/main/$command"
+		;;
+
 	"processRegister")
 		pid=$3
+		if [ -z "$pid" ]
+		then
+			echo "Third argument should be PID!"
+			exit
+		fi
 
-		machineId=$() #mozda preko nekog filea pohraniti id koji vrati machineRegister?
+		machineId=$(cat machineId | grep MachineId | cut -d ' ' -f2) #mozda preko nekog filea pohraniti id koji vrati machineRegister?
+		if [ -z "$machineId" ]
+		then
+			echo "MachineId not found!"
+			exit
+		fi
 
 		processName=$(cat /proc/$pid/status | grep Name | cut -d$'\t' -f2)
 		processPath=$(ps -p $pid -o cmd | cut -d$'\n' -f2)
 
-		curl -s -X POST -H "Content-type: application/json" \
-			-d "{\"processIdSystem\":\"$pid\", \"name\":\"$processName\", \"machineId\":\"$machineId\"}" \
-			--insecure https://$ip_address/api/main/processRegister
+		newProcessId=$(curl -s -X POST -H "Content-type: application/json" \
+			-d "{\"processIdSystem\":\"$pid\", \"name\":\"$processName\", \"machineId\":\"$machineId\", \"processPath\":\"$processPath\"}" \
+			--insecure https://$ip_address/api/main/processRegister)
 
-		echo "$ip_address/api/main/$command" ;;
+		printf "%s: %s" $pid $newProcessId >> machineId
+
+		echo $newProcessId$'\n'
+		echo "$ip_address/api/main/$command"
+		;;
+
 	"processUpdate")
 		pid=$3
+		if [ -z "$pid" ]
+		then
+			echo "Third argument should be PID!"
+			exit
+		fi
 
-		state=$(cat /proc/$pid/status | grep Status | cut -d$'\t' -f2)
-		cpuUtil=$(ps -p $pid -o %cpu | cut -d$'\n' -f2)
-		memUtil=$(ps -p $pid -o %mem | cut -d$'\n' -f2)
+		processId=$(cat machineId | grep $pid: | cut -d ' ' -f2)
+		if [ -z "$processId" ]
+		then
+			echo "ProcessId not found!"
+			exit
+		fi
+
+		state=$(cat /proc/$pid/status | grep State | cut -d$'\t' -f2)
+		cpuUtil=$(ps -p $pid -o %cpu | cut -d$'\n' -f2 | xargs)
+		memUtil=$(ps -p $pid -o %mem | cut -d$'\n' -f2 | xargs)
 		threads=$(cat /proc/$pid/status | grep Threads | cut -d$'\t' -f2)
 
-		curl -s -X POST -H "Content-type: application/json" \
-			-d "{\"processId\":\"$pid\", \"state\":\"$state\", \"cpuUtil\":\"$cpuUtil\", \"threads\":\"$threads\"}"
-			--insecure https://$ip_address/api/main/processUpdate
+		echo $processId
+		echo $state$'\n'$cpuUtil$'\n'$memUtil$'\n'$threads$'\n'
 
-		echo "$ip_address/api/main/$command" ;;
+		updatedProcessId=$(curl -s -X POST -H "Content-type: application/json" \
+			-d "{\"processId\":\"$processId\", \"state\":\"$state\", \"cpuUtil\":$cpuUtil, \"memUtil\":$memUtil, \"threads\":$threads}" \
+			--insecure https://$ip_address/api/main/processUpdate)
+
+		echo $updatedProcessId$'\n'
+		echo "$ip_address/api/main/$command"
+		;;
 	*)
-		echo "Unknown command!" ;;
+		echo "Unknown command!"
+		;;
 esac
